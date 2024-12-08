eebf3db399edffe92f03c230510a14e92d25bafb
sidney-HC <sidney.howardclinton@gmail.com>
2024-12-07 23:03:57 -0500

    update

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ app.get('/help', (req, res) => {
 app.get('/about', (req, res) => {
     res.sendFile('public/about_page.html', { root: __dirname })
 })
-
+app.use((req, res) => {
+    res.status(404).send('404: Page Not Found');
+});
+si
 app.listen(port, hostname, () => {
     console.log(`Express app listening on Port: ${port}`)
 })
